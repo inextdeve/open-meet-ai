59ddfe206d5e119f58a1c8b04de4b14835859be1
Yassine <yassinefarroud@protonmail.com>
2026-05-19 20:19:32 +0100

    agents data table

---
+++ src/modules/agents/ui/components/columns.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { AgentGetOne } from "../../types";
+import { GeneratedAvatar } from "@/components/generated-avatar";
+import { CornerDownRightIcon, VideoIcon } from "lucide-react";
+import { Badge } from "@/components/ui/badge";
+
+export const columns: ColumnDef<AgentGetOne>[] = [
+  {
+    accessorKey: "name",
+    header: "Agent Name",
+    cell: ({ row }) => (
+      <div className="flex flex-col gap-y-1">
+        <div className="flex items-center gap-x-2">
+          <GeneratedAvatar
+            seed={row.original.name}
+            variant="botttsNeutral"
+            className="size-6"
+          />
+          <span className="font-semibold capitalize">{row.original.name}</span>
+        </div>
+        <div className="flex items-center gap-x-2">
+          <CornerDownRightIcon className="size-3 text-muted-foreground" />
+          <div className="text-sm text-muted-foreground truncate capitalize max-w-[200px]">
+            {row.original.instructions}
+          </div>
+        </div>
+      </div>
+    ),
+  },
+  {
+    accessorKey: "meetingCount",
+    header: "Meetings",
+    cell: ({ row }) => (
+      <Badge variant="outline" className="flex items-center gap-x-2">
+        <VideoIcon className="text-blue-700" />
+        {row.original.meetingCount}{" "}
+        {row.original.meetingCount === 1 ? "5 Meeting" : "5 Meetings"}
+      </Badge>
+    ),
+  },
+  {
+    accessorKey: "amount",
+    header: "Amount",
+  },
+];

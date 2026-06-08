7ac4b6e626e5c5c9b82f2b0ad2b6ced11eefcfa5
Yassine <yassinefarroud@protonmail.com>
2026-06-08 14:14:24 +0100

    Add component for completed meeting state

--- src/modules/meetings/ui/views/meetings-id-view.tsx
+++ src/modules/meetings/ui/views/meetings-id-view.tsx
@@ -19,6 +19,7 @@ import { UpcomingState } from "../components/upcoming-state";
 import { ActiveState } from "../components/active-state";
 import { CancelledState } from "../components/cancelled-state";
 import { ProcessingState } from "../components/processing-state";
+import { CompletedState } from "../components/completed-state";
 
 interface Props {
   meetingId: string;
@@ -87,7 +88,7 @@ export const MeetingIdView = ({ meetingId }: Props) => {
             isCancelling={false}
           />
         )}
-        {isCompleted && <div>completed</div>}
+        {isCompleted && <CompletedState data={data} />}
         {isProcessing && <ProcessingState />}
       </div>
     </>
